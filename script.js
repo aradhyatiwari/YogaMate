1e6f67e62ecfa1447d7181a745b2f62a0ba98e41
aradhyatiwari <aradhya.prad@gmail.com>
2022-02-12 21:37:29 +0530

    Update script.js
    Kabhi kabhi code phat rha hai :-(

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 
+
 //by Aradhya Tiwari
 let img = document.getElementById("img");
-let name = document.getElementById("name");
+let namee = document.getElementById("name");
 let obj = JSON.parse(JSON.stringify(data))
+let timing = document.getElementById("timing");
+
 class yog{
     constructor(pose){
+        let timee = 30;
+        this.timee = timee;
         this.pose = pose
         let current = this.pose
         this.current = current
     }
-    main(){
-        if(this.pose == 10) return
-        console.log(obj.result[this.pose].id);
-        this.pose++
-        img.src=obj.result[this.pose].img_url
-        name.innerHTML = obj.result[this.pose].sanskrit_name
-        setTimeout(()=>{this.main()}, 2000);
+    timer(){
+      setInterval(()=>{
+            this.timee--;
+            timing.innerHTML=(this.timee);
+            if(this.timee==0){
+                this.timee += 30;
+                this.update();
+            }
+        },1000)
+    }
+    update(){
+        this.pose++;
+        img.src=obj.result[this.pose].img_url;
+        namee.innerHTML = obj.result[this.pose].sanskrit_name;
+        clearInterval(this.timer());
+        this.timer();
     }
     next(){
         this.pose+=1
@@ -25,11 +39,9 @@ class yog{
     }
     reset(){
         this.pose = this.current
-        this.main()
+        this.timer()
     }
 }
 
-let gar = new yog(0);
-gar.main();
-
-
+let gar = new yog(1);
+gar.update();
